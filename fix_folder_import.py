2811d91679b9b1a27de0b2353ed412f316bb0ce9
AdityasahuX07 <smily998800@gmail.com>
2026-08-31 16:43:39 +0530

    Remove irrelevant files from PR

--- fix_folder_import.py
+++
@@ -1,7 +0,0 @@
-with open('composeApp/src/commonMain/kotlin/com/nuvio/app/features/collection/FolderDetailScreen.kt', 'r', encoding='utf-8') as f:
-    c = f.read()
-
-if 'import com.nuvio.app.features.home.HomeFocusCoordinator' not in c:
-    c = c.replace('import androidx.compose.runtime.Composable', 'import androidx.compose.runtime.Composable\\nimport com.nuvio.app.features.home.HomeFocusCoordinator')
-    with open('composeApp/src/commonMain/kotlin/com/nuvio/app/features/collection/FolderDetailScreen.kt', 'w', encoding='utf-8') as f:
-        f.write(c)
